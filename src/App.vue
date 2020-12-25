c0510691a16681cf368f988ec945d8d3cbbca0b3
Nicolas G Esser <nicolas@Nicolass-MacBook-Air.local>
2020-12-25 13:25:33 -0600

    Initial commit.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <Header />
+    </div>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/layout/Header";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Header,
+  },
+};
 </script>
 
 <style>
@@ -23,6 +25,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="header">
+    <h1>ToDo List</h1>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "header",
+};
+</script>
+
+<style scoped>
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.header {
+  background: #333;
+  color: #fff;
+  text-align: center;
+  padding: 30px;
+}
+
+.header a {
+  color: #fff;
+  padding-right: 5px;
+}
+</style>
